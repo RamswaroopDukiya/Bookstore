c17ef83a619ab0da95d2a70c603d18c73aabf084
Ramswaroop Dukiya <ramswrup.dukiya@gmail.com>
2024-08-08 21:48:39 +0530

    BookStore

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <hr />
+      <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
+        
+        <aside>
+        Copyright @ 2024, BookStore, Ram - All Right Reserved.
+        </aside>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
